26f04d2c3eecd1ef9a1404d6cda4f9ee1f7c63b6
yangbin1 <yangbin199191@163.com>
2019-08-21 22:15:00 +0800

    2

---
+++ src/com/yangbin1/search/BinarySearch.java
@@ -0,0 +1,26 @@
+package com.yangbin1.search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 10, 89, 1000, 1234};
+    }
+
+    public static int binarySearch(int[] arr,int left,int right,int value) {
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+
+        if (value > mid) {
+            return binarySearch(arr, mid + 1, right, value);
+        } else if (value < mid) {
+            return binarySearch(arr, left, mid - 1, value);
+        } else {
+            return mid;
+        }
+
+    }
+
+}
